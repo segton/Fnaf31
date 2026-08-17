268074f435a771a8f7c8ceae6cb7787148ce2fcd
segton <shuseanlimy@gmail.com>
2026-08-17 14:56:34 +0800

    added camera class

--- Fnaf31/Game.h
+++ Fnaf31/Game.h
@@ -1,4 +1,34 @@
 #pragma once
+
+enum class GameState
+{
+	Menu,
+	Playing,
+	Win,
+	Lose,
+};
+
+struct OfficeState
+{
+	bool leftDoorClosed = false;
+	bool rightDoorClosed = false;
+	bool leftLightOn = false;
+	bool rightLightOn = false;
+};
+
+struct CameraState
+{
+	bool isCameraPanelOpen = false;
+	bool isCameraActive = false;
+};
+
+struct NightState
+{
+	int currentHour = 12;
+	int currentMinute = 0;
+	int currentSecond = 0;
+};
+
 class Game
 {
 public:
